6b0bc22e5898ca5abe858a2643c63f19a4e1e3ac
Jonathan Jin <jjin082693@gmail.com>
2013-12-28 21:32:38 +0800

    use choice for random element selection

--- dogesay.py
+++ dogesay.py
@@ -5,7 +5,6 @@ from random   import randrange, choice
 
 DOGE_PREFIXES   = ["such", "much", "so", "many", "wow", "very"]
 DOGE_EJACULATES = ["wow"]
-
 DOGE_FACE_PATHS = {"norm" : "static/doge.txt",
                    "ascii": "static/doge_ascii.txt"}
 
@@ -16,7 +15,7 @@ def doge_syntax(clause):
     if len(clause.split()) > 1:
         return clause
     else:
-        return DOGE_PREFIXES[randrange(0,len(DOGE_PREFIXES))]+" "+clause
+        return choice(DOGE_PREFIXES)+" "+clause
 
 def insert(clause, img_file):
     img_file[randrange(0,len(img_file))] += (" " + clause)
@@ -39,12 +38,11 @@ if __name__ == "__main__":
 
     for clause in clauses_file:
         clause = random_whitespace()+doge_syntax(clause.strip())
-        move_next_iter = False
 
+        move_next_iter = False
         while not move_next_iter:
             if randrange(0,10) > WOW_CHANCE:
-                insert(DOGE_EJACULATES[randrange(0,len(DOGE_EJACULATES))],
-                       doge_face_file) 
+                insert(choice(DOGE_EJACULATES), doge_face_file) 
             move_next_iter = True
 
         insert(clause, doge_face_file)
