02e89a04e6dfce329f6ba634c9e91ba2f2804560
Jonathan Jin <jjin082693@gmail.com>
2013-12-28 21:29:13 +0800

    chug chug chug

--- dogesay.py
+++ dogesay.py
@@ -3,44 +3,51 @@
 from argparse import ArgumentParser
 from random   import randrange, choice
 
-DOGE_PREFIXES = ["such", "much", "so", "many", "wow"]
+DOGE_PREFIXES   = ["such", "much", "so", "many", "wow", "very"]
 DOGE_EJACULATES = ["wow"]
 
-DOGE_FACE_FILE = open("static/doge.txt","r").read().splitlines()
+DOGE_FACE_PATHS = {"norm" : "static/doge.txt",
+                   "ascii": "static/doge_ascii.txt"}
 
 WOW_CHANCE = 5
-
-def num_words(clause):
-    return len(clause.split())
+MAX_WHITESPACE = 15
 
 def doge_syntax(clause):
-    if num_words(clause) > 1:
+    if len(clause.split()) > 1:
         return clause
     else:
         return DOGE_PREFIXES[randrange(0,len(DOGE_PREFIXES))]+" "+clause
 
 def insert(clause, img_file):
     img_file[randrange(0,len(img_file))] += (" " + clause)
+
+def random_whitespace():
+    return randrange(0,MAX_WHITESPACE)*" "
     
 if __name__ == "__main__":
     parser = ArgumentParser(description="Cowsay for a new generation.")
     parser.add_argument("inputfile", metavar="<input file>")
-    parser.add_argument("-a", "--ascii", action="store_true", help="Use ASCII doge")
+    parser.add_argument("-a", "--ascii", action="store_true",
+                        help="Use ASCII doge")
 
-    clauses_file = open(parser.parse_args().inputfile, "r")
+    args = parser.parse_args()
+
+    doge_face_path = DOGE_FACE_PATHS["ascii" if args.ascii else "norm"]
+    doge_face_file = open(doge_face_path, "r").read().splitlines()
+
+    clauses_file = open(args.inputfile, "r")
 
     for clause in clauses_file:
-        clause = clause.strip()
+        clause = random_whitespace()+doge_syntax(clause.strip())
         move_next_iter = False
 
         while not move_next_iter:
             if randrange(0,10) > WOW_CHANCE:
                 insert(DOGE_EJACULATES[randrange(0,len(DOGE_EJACULATES))],
-                       DOGE_FACE_FILE) 
+                       doge_face_file) 
             move_next_iter = True
 
-        clause = doge_syntax(clause)
-        insert(clause, DOGE_FACE_FILE)
+        insert(clause, doge_face_file)
 
-    for line in DOGE_FACE_FILE:
+    for line in doge_face_file:
         print(line)
