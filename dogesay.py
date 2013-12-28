07fe66dae8f018c97f8a67ebaaf40a53b2338f0c
Jonathan Jin <jjin082693@gmail.com>
2013-12-28 21:45:55 +0800

    implemented (kind of hacky) non-repeat random line selection

--- dogesay.py
+++ dogesay.py
@@ -10,15 +10,25 @@ DOGE_FACE_PATHS = {"norm" : "static/doge.txt",
 
 WOW_CHANCE = 5
 MAX_WHITESPACE = 15
+MIN_WHITESPACE = 2
 
 def doge_syntax(clause):
     return clause if len(clause.split())>1 else choice(DOGE_PREFIXES)+" "+clause
 
+used_indices = []
+def random_select_no_repeat(max, ref_pool):
+    index =  randrange(0,max)
+    while index in ref_pool:
+        index =  randrange(0,max)
+    ref_pool.append(index)
+    return index
+    
 def random_insert_clause(clause, img_file):
-    img_file[randrange(0,len(img_file))] += clause
+    # img_file[randrange(0,len(img_file))] += clause
+    img_file[random_select_no_repeat(len(img_file), used_indices)] += clause
 
 def random_whitespace():
-    return randrange(0,MAX_WHITESPACE)*" "
+    return randrange(MIN_WHITESPACE, MAX_WHITESPACE)*" "
     
 if __name__ == "__main__":
     parser = ArgumentParser(description="Cowsay for a new generation.")
@@ -39,7 +49,8 @@ if __name__ == "__main__":
         move_next_iter = False
         while not move_next_iter:
             if randrange(0,10) > WOW_CHANCE:
-                random_insert_clause(choice(DOGE_EJACULATES), doge_face_file) 
+                random_insert_clause(random_whitespace()+choice(DOGE_EJACULATES),
+                                     doge_face_file) 
             move_next_iter = True
 
         random_insert_clause(clause, doge_face_file)
